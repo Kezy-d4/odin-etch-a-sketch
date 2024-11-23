6e7ccf410c8aa63eec7c67d4987f497ef4438095
David H <151378521+Kezy-d4@users.noreply.github.com>
2024-11-23 18:51:12 -0500

    feat: add function to generate grid of squares

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 let container = document.querySelector('#container');
+let grid = document.querySelector('.grid');
+
+function constructGrid(dimensions) {
+  for (let rowCounter = 1; rowCounter <= dimensions; rowCounter++) { 
+    let row = document.createElement('div');
+
+    for (let squareCounter = 1; squareCounter <= dimensions; squareCounter++) {
+      let square = document.createElement('div');
+      square.textContent = squareCounter;
+      square.className = 'square';
+      row.appendChild(square);
+    }
+
+    row.className = 'row';
+    grid.appendChild(row);
+  }
+
+  let numberOfSquares = document.querySelectorAll('.square');
+  console.log(numberOfSquares.length);
+}
+
+constructGrid(16);
